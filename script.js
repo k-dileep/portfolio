c62f10ba36ec8f90c6f5ea248c8b6592e0ce0351
K DILEEP <153375962+k-dileep@users.noreply.github.com>
2024-11-26 22:20:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function downloadCV() {
-    window.location.href = 'https://drive.google.com/uc?export=download&id=1IyMZLaQzhHbBVCLXVHM9rl-jMA7u2WWj';
+    window.location.href = 'https://drive.google.com/file/d/1IyMZLaQzhHbBVCLXVHM9rl-jMA7u2WWj/view?usp=drive_link';
   }
   
 
